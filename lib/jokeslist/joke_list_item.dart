a3501e7a975d53139aaf64015a4d49dc7cc70155
Anıl Ulak <anil_ulak@hotmail.com>
2018-05-08 16:00:57 +0300

    Navigation from jokes list to a joke (when clicked to a joke_list_item) is added.

--- lib/jokeslist/joke_list_item.dart
+++ lib/jokeslist/joke_list_item.dart
@@ -9,6 +9,10 @@ class JokeListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new ListTile(
+        onTap: () {
+          print('hello from joke_list_item');
+          Navigator.of(context).pushNamed("/Joke");
+        },
         leading: new CircleAvatar(child: new Text(_jokeModal.categoryId.toString())),
         title: new Text(_jokeModal.jokeId.toString()),
         subtitle: new Text(_jokeModal.jokeHeader));
