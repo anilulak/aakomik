62640dcd828621069d5a49925af7571f859f014f
Anıl Ulak <anil_ulak@hotmail.com>
2018-06-02 23:10:32 +0300

    Choosing and seeing a joke from favourites list is added.

--- lib/favouriteslist/favourite_list_item.dart
+++ lib/favouriteslist/favourite_list_item.dart
@@ -1,17 +1,68 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:aakomik/selectedjoke.dart';
 import 'package:flutter/material.dart';
 import 'package:aakomik/favouriteslist/modal/favourite.dart';
+import 'package:path_provider/path_provider.dart';
 
 class FavouriteListItem extends StatelessWidget {
   final FavouriteModal _favouriteModal;
 
   FavouriteListItem(this._favouriteModal);
 
+  Future<String> get _localPathForFavourites async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFileForFavourites async {
+    final path = await _localPathForFavourites;
+
+    return new File('$path/favourites.txt');
+  }
+
+  Future<File> writeRestFavourites(String text) async {
+    final file = await _localFileForFavourites;
+
+    // Write the file
+    return file.writeAsString('$text', mode: FileMode.WRITE);
+  }
+
+  Future<List<String>> readFavourites() async {
+    try {
+      final file = await _localFileForFavourites;
+
+      // Read the file
+      String contents = await file.readAsString();
+      print('Contents of read favourites:' + contents);
+      return divideJokes(contents);
+    } catch (e) {
+      // If we encounter an error, return error, this is our default message.
+      return divideJokes("");
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
+
+
+
     return new ListTile(
         onTap: () {
           print('fikra will be opened');
-          Navigator.of(context).pushNamed("/Joke");
+          Navigator.push(
+            context,
+            new MaterialPageRoute(
+              builder: (context) => new SelectedJoke(
+                category_id: _favouriteModal.categoryId.toString(),
+                joke_id: _favouriteModal.jokeId.toString(),
+              ),
+            ),
+          );
         },
         leading: new IconButton(
           icon: new Icon(Icons.favorite),
@@ -23,4 +74,15 @@ class FavouriteListItem extends StatelessWidget {
         //subtitle: new Text(_favouriteModal.jokeText)
     );
   }
+}
+
+
+List<String> divideJokes(String text) {
+  List<String> jokes = text.trim().split("#");
+  return jokes;
+}
+
+List<String> divideOneJoke(String text) {
+  List<String> jokeElements = text.trim().split("*");
+  return jokeElements;
 }
